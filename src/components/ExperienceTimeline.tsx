3a377003a91670ca23697a98c7ee261beaf7b6bb
MarcelMatsal <marcel_mateos_salles@brown.edu>
2026-03-12 16:44:21 -0400

    small change to the timeline

--- src/components/ExperienceTimeline.tsx
+++ src/components/ExperienceTimeline.tsx
@@ -7,7 +7,26 @@ export default function TimelineSection() {
   return (
     <section id="experience" className="py-20">
       <h2 className="text-3xl font-bold mb-8 text-center">Experience</h2>
-      <VerticalTimeline animate={true} lineColor="rgb(164, 208, 244)">
+      <VerticalTimeline animate={true} lineColor="#6B9FD4">
+
+        {/* Head TA CSCI1460 */}
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: '#99C1FF', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid #99C1FF' }}
+          date="Dec 2025 - Present"
+          dateClassName="text-gray-900 dark:text-white font-bold"
+          iconStyle={{ background: '#99C1FF', color: '#fff' }}
+          icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="currentColor" width="24" height="24">
+            <path d="M160 64c0-35.3 28.7-64 64-64H576c35.3 0 64 28.7 64 64V352c0 35.3-28.7 64-64 64H336.8c-11.8-25.5-29.9-47.5-52.4-64H384V320c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v32h64V64L224 64v49.1C205.2 102.2 183.3 96 160 96V64zm0 64a96 96 0 1 1 0 192 96 96 0 1 1 0-192zM133.3 352h53.3C260.3 352 320 411.7 320 485.3c0 14.7-11.9 26.7-26.7 26.7H26.7C11.9 512 0 500.1 0 485.3C0 411.7 59.7 352 133.3 352z"/>
+          </svg>}
+        >
+          <h3 className="vertical-timeline-element-title">Head Teaching Assistant</h3>
+          <h4 className="vertical-timeline-element-subtitle">CSCI1460</h4>
+          <p className="text-white font-bold">Brown University</p>
+        </VerticalTimelineElement>
+
+
         {/* Timeline Element 1 */}
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
@@ -21,7 +40,7 @@ export default function TimelineSection() {
           <h3 className="vertical-timeline-element-title">Software Engineer Intern</h3>
           <h4 className="vertical-timeline-element-subtitle">Pinterest</h4>
           <p className="text-white font-bold">Backend - Observability Team</p>
-          <p className="text-white font-bold">Developing GenAI tools (such as agents, MCP servers, anomaly detection) for all engineers at Pinterest.</p>
+          <p className="text-white font-bold">Developed GenAI tools (such as agents, MCP servers, anomaly detection) for all engineers at Pinterest.</p>
         </VerticalTimelineElement>
         
 
@@ -41,6 +60,7 @@ export default function TimelineSection() {
           <h4 className="vertical-timeline-element-subtitle">CSCI 1470: Deep Learning</h4>
           <p className="text-white font-bold">Brown University</p>
         </VerticalTimelineElement>
+
 
         {/* Timeline Element 3 */}
         <VerticalTimelineElement
@@ -130,7 +150,7 @@ export default function TimelineSection() {
             <path d="M243.4 2.6l-224 96c-14 6-21.8 21-18.7 35.8S16.8 160 32 160v8c0 13.3 10.7 24 24 24H456c13.3 0 24-10.7 24-24v-8c15.2 0 28.3-10.7 31.3-25.6s-4.8-29.9-18.7-35.8l-224-96c-8.1-3.4-17.2-3.4-25.2 0zM128 224H64V420.3c-.6 .3-1.2 .7-1.8 1.1l-48 32c-11.7 7.8-17 22.4-12.9 35.9S17.9 512 32 512H480c14.1 0 26.5-9.2 30.6-22.7s-1.1-28.1-12.9-35.9l-48-32c-.6-.4-1.2-.7-1.8-1.1V224H384V416H344V224H280V416H232V224H168V416H128V224zm128-96c-17.7 0-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32s-14.3 32-32 32z"/>
           </svg>}
         >
-          <h3 className="vertical-timeline-element-title">Incoming Senior studying Computer Science and Economics</h3>
+          <h3 className="vertical-timeline-element-title">Senior studying Computer Science and Economics</h3>
           <h4 className="vertical-timeline-element-subtitle">Brown University</h4>
           <p className="text-white font-bold">GPA: 4.0</p>
         </VerticalTimelineElement>
